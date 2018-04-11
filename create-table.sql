e5eaef8cc00e613b07ac841a4fbaeaabfc3d68e5
Tomas Vondra <tomas.vondra@2ndquadrant.com>
2018-04-12 01:06:07 +0200

    update the talk for pgconf.de 2018

---
+++ create-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE zip_codes (
+  country_code      VARCHAR(2),
+  postal_code       VARCHAR(20),
+  place_name        VARCHAR(180),
+  state_name        VARCHAR(100),
+  stat_code         VARCHAR(20),
+  county_name       VARCHAR(100),
+  county_code       VARCHAR(20),
+  community_name    VARCHAR(100),
+  community_code    VARCHAR(20),
+  latitude          REAL,
+  longitude         REAL,
+  accuracy          REAL
+);
--- queries.sql
+++ queries.sql
@@ -1,62 +1,58 @@
-CREATE TABLE zip_codes (
-    zip_code   INT PRIMARY KEY,
-    city       TEXT,
-    state      TEXT,
-    county     TEXT,
-    latitude   REAL,
-    longitude  REAL
-);
-
--- shell command
-COPY zip_codes FROM '/tmp/no_postal_codes_utf.csv' WITH (format csv, header true);
-
 ANALYZE zip_codes;
 
 -- single-column condition
 EXPLAIN (ANALYZE, TIMING off)
-SELECT * FROM zip_codes WHERE city = 'Oslo';
+SELECT * FROM zip_codes WHERE place_name = 'Berlin';
 
 -- number of tuples / pages in a table
 SELECT reltuples, relpages FROM pg_class WHERE relname = 'zip_codes';
 
 -- statistics for a table
-SELECT * FROM pg_stats WHERE tablename = 'zip_codes' AND attname = 'city';
+SELECT * FROM pg_stats WHERE tablename = 'zip_codes' AND attname = 'place_name';
 
 -- multi-column condition
 EXPLAIN (ANALYZE, TIMING off)
-SELECT * FROM zip_codes WHERE city = 'Oslo' AND county = 'Oslo';
+SELECT * FROM zip_codes WHERE place_name = 'Berlin'
+                          AND state_name = 'Berlin';
 
 -- multi-column condition, inequality
 EXPLAIN (ANALYZE, TIMING off)
-SELECT * FROM zip_codes WHERE city = 'Oslo' AND county != 'Oslo';
+SELECT * FROM zip_codes WHERE place_name = 'Berlin'
+                          AND state_name != 'Berlin';
 
 -- multi-column condition, incompatible conditions
 EXPLAIN (ANALYZE, TIMING off)
-SELECT * FROM zip_codes WHERE city = 'Oslo' AND county = 'Halden';
+SELECT * FROM zip_codes WHERE place_name = 'Berlin'
+                          AND state_name = 'Bayern';
 
 -- functional dependencies
 CREATE STATISTICS s (dependencies)
-    ON city, state, county FROM zip_codes;
+    ON place_name, state_name, county_name FROM zip_codes;
 
 ANALYZE zip_codes;
 
 SELECT stxdependencies FROM pg_statistic_ext WHERE stxname = ‘s’;
 
 -- ndistinct
-EXPLAIN (ANALYZE, TIMING off) SELECT 1 FROM zip_codes GROUP BY county;
+EXPLAIN (ANALYZE, TIMING off)
+SELECT 1 FROM zip_codes GROUP BY community_name;
 
-EXPLAIN (ANALYZE, TIMING off) SELECT 1 FROM zip_codes GROUP BY state;
+EXPLAIN (ANALYZE, TIMING off)
+SELECT 1 FROM zip_codes GROUP BY county_name;
+
+EXPLAIN (ANALYZE, TIMING off)
+SELECT 1 FROM zip_codes GROUP BY state_name;
 
 -- number of distinct groups
 SELECT attname, n_distinct
   FROM pg_stats WHERE tablename = 'zip_codes';
 
-EXPLAIN (ANALYZE, TIMING off) SELECT 1 FROM zip_codes GROUP BY state, county;
+EXPLAIN (ANALYZE, TIMING off)
+SELECT 1 FROM zip_codes GROUP BY state_name, county_name, community_name;
 
 CREATE STATISTICS s (ndistinct)
-    ON county, state, city
+    ON state_name, county_name, community_name
   FROM zip_codes;
-ANALYZE zip_codes;
 
 SELECT stxndistinct FROM pg_statistic_ext;
 
