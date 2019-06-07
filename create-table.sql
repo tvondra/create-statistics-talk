348f7d6742175f51711cb4e47769d522fad2878d
Tomas Vondra <tomas.vondra@2ndquadrant.com>
2019-06-07 23:35:05 +0200

    update slides for Lviv meetup

--- create-table.sql
+++ create-table.sql
@@ -3,7 +3,6 @@ CREATE TABLE zip_codes (
   place_name        VARCHAR(180),
   state_name        VARCHAR(100),
   province_name     VARCHAR(100),
-  community_name    VARCHAR(100),
   latitude          REAL,
   longitude         REAL
 );
